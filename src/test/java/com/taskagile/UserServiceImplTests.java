68bf38551868e355293a6bd4506a3788d852a3df
03enje <03enje@gmail.com>
2022-11-04 22:46:10 +0900

    유저 서비스 구현체 테스트

---
+++ src/test/java/com/taskagile/UserServiceImplTests.java
@@ -0,0 +1,85 @@
+package com.taskagile;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.taskagile.domain.application.commands.RegistrationCommand;
+import com.taskagile.domain.application.impl.UserServiceImpl;
+import com.taskagile.domain.model.user.EmailAddressExistsException;
+import com.taskagile.domain.model.user.RegistrationException;
+import com.taskagile.domain.model.user.UsernameExistsException;
+
+public class UserServiceImplTests {
+  private RegistrationManagement registrationManagementMock;
+  private DomainEventPublisher eventPublisherMock;
+  private MailManager mailManagerMock;
+  private UserServiceImpl instance;
+
+  @BeforeEach
+  public void setUp() {
+    registrationManagementMock = mock (registrationManagement.class);
+    eventPublisherMock = mock (DomainEventPublisher.class);
+    mailManagerMock = mock (MailManager.class);
+    instance = new UserServiceImpl(registrationManagementMock, eventPublisherMock, mailManagerMock);
+  }
+
+  @Test
+  public void register_nullCommand_shouldFail() throws RegistrationException {
+    assertThrows(IllegalArgumentException.class, () -> {
+      instance.register(null);
+    });
+  }
+
+  @Test
+  public void register_existingUsername_shouldFail() throws RegistrationException {
+    String username = "existing";
+    String emailAddress = "sunny@taskagile.com";
+    String password = "MyPassword!";
+    doThrow(UsernameExistsException.class).when(registrationManagementMock)
+      .register(username, emailAddress, password);
+
+    RegistrationCommand command = new RegistrationCommand(username, emailAddress, password);
+    assertThrows(RegistrationException.class, () -> {
+      instance.register(command);
+    });
+  }
+
+  @Test
+  public void register_existingEmailAddress_shouldFail() throws RegistrationException {
+    String username = "sunny";
+    String emailAddress = "existing@taskagile.com";
+    String password = "MyPassword!";
+    doThrow(EmailAddressExistsException.class).when(registrationManagementMock)
+      .register(username, emailAddress, password);
+
+    RegistrationCommand command = new RegistrationCommand(username, emailAddress, password);
+    assertThrows(RegistrationException.class, () -> {
+      instance.register(command);
+    });
+  }
+
+  @Test
+  public void register_validCommand_shouldSucceed() throws RegistrationException {
+    String username = "sunny";
+    String emailAddress = "sunny@taskagile.com";
+    String password = "MyPassword!";
+    User newUser = User.create(username, emailAddress, password);
+    when(registrationManagementMock.register(username, emailAddress, password))
+      .thenReturn(newUser);
+    RegistrationCommand command = new RegistrationCommand(username, emailAddress, password);
+
+    instance.register(command);
+
+    verify(mailManagerMock).send(
+      emailAddress,
+      "Welcome to TaskAgile",
+      "welcome.ftl",
+      MessageVariable.from("user", newUser)
+    );
+    verify(domainEventPublisherMock).publish(new UserRegisteredEvent(newUser));
+  }
+}
