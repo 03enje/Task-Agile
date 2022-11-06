3309b30d989dbffbc87c5b70bf852f13c845f4bb
03enje <03enje@gmail.com>
2022-11-06 23:58:47 +0900

    유저서비스구현체 구현

--- src/main/java/com/taskagile/domain/application/impl/UserServiceImpl.java
+++ src/main/java/com/taskagile/domain/application/impl/UserServiceImpl.java
@@ -1,14 +1,48 @@
 package com.taskagile.domain.application.impl;
 
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import com.taskagile.MailManager;
 import com.taskagile.domain.application.UserService;
 import com.taskagile.domain.application.commands.RegistrationCommand;
+import com.taskagile.domain.common.event.DomainEventPublisher;
 import com.taskagile.domain.model.user.RegistrationException;
+import com.taskagile.domain.model.user.UserRegisteredEventHandler;
 
+@Service
+@transactional
 public class UserServiceImpl implements UserService{
+  private RegistrationManagement registrationManagement;
+  private DomainEventPublisher domainEventPublisher;
+  private MailManager mailManager;
+
+  public UserServiceImpl(RegistrationManagement registrationManagement,
+                          DomainEventPublisher domainEventPublisher,
+                          MailManager mailManager) {
+    this.registrationManagement = registrationManagement;
+    this.domainEventPublisher = domainEventPublisher;
+    this.mailManager = mailManager;
+  }
 
   @Override
   public void register(RegistrationCommand command) throws RegistrationException {
-    // TODO implement this
+    Assert.notNull(command, "Parameter `command` must not be null");
+    User newUser = registrationManagement.register(
+      command.getUsername();
+      command.getEmailAddress();
+      command.getPassword());
+
+    sendWelcomeMessage(newUser);
+    domainEventPublisher.publish(new UserRegisteredEvent(newUser));
   }
 
+  private void sendWelcomeMessage(User user) {
+    mailManager.send(
+      user.getEmailAddress(),
+      "Welcome to TaskAgile",
+      "welcome.ftl",
+      MessageVariable.from("user", user)
+    );
+  }
 }
