2613564703ed4e07cb5cee1103428d18746d3e45
03enje <03enje@gmail.com>
2022-11-10 22:20:50 +0900

    회원가입 관리 테스트 완성

--- src/test/java/com/taskagile/RegistrationManagementTests.java
+++ src/test/java/com/taskagile/RegistrationManagementTests.java
@@ -1,5 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.verify;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties.Registration;
 
 import com.taskagile.domain.model.user.RegistrationException;
 
@@ -26,4 +33,50 @@ public class RegistrationManagementTests {
     when(repositoryMock.findByUsername(username)).thenReturn(new User());
     instance.register(username, emailAddress, password);
   }
+
+  @Test
+  public void register_existedEmailAddress_shouldFail() throws RegistrationException {
+    String username = "sunny";
+    String emailAddress = "existed@taskagile.com";
+    String password = "MyPassword";
+    when(repositoryMock.findByEmailAddress(emailAddress)).thenReturn(new User());
+    instance.register(username, emailAddress, password);
+  }
+
+  @Test
+  public void register_uppercaseEmailAddress_shouldSucceedAndBecomeLowercase() throws RegistrationException {
+    String username = "sunny";
+    String emailAddress = "Sunny@TaskAgile.com";
+    String password = "MyPassword!";
+    instance.register(username, emailAddress, password);
+    User userToSave = User.create(username, emailAddress.toLowerCase(), password);
+    verify(repositoryMock).save(userToSave);
+  }
+
+  @Test
+  public void register_newUser_shouldSucceed() throws RegistrationException {
+    String username = "sunny";
+    String emailAddress = "sunny@taskagile.com";
+    String password = "MyPassword!";
+    String encryptedPassword = "EncryptedPassword";
+    User newUser = User.create(username, emailAddress, encryptedPassword);
+
+    // 리파지토리 목 설정
+    // 사용자가 존재하지 않음을 나타내고자 null 값 반환
+    when(repositoryMock.findByUsername(username)).thenReturn(null);
+    when(repositoryMock.findByEmailAddress(emailAddress)).thenReturn(null);
+    doNothing().when(repositoryMock).save(newUser);
+    // passwordEncryptor 목 설정
+    when(passwordEncryptorMock.encrypt(password)).thenReturn("EncryptedPassword");
+
+    User savedUser = instance.register(username, emailAddress, password);
+    InOrder inOrder = inOrder(repositoryMock);
+    inOrder.verify(repositoryMock).findByUsername(username);
+    inOrder.verify(repositoryMock).findByEmailAddress(emailAddress);
+    inOrder.verify(repositoryMock).save(newUser);
+    verify(passwordEncryptorMock).encrypt(password);
+    assertEquals("saved user's password should be encrypted", encryptedPassword, savedUser.getPassword());
+  }
+
+
 }
