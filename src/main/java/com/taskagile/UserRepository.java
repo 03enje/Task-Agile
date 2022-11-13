6798a7ea33021adc169a96aa34a2b9ba00ac5b87
03enje <03enje@gmail.com>
2022-11-13 22:44:52 +0900

    유저 레포지토리 수정

--- src/main/java/com/taskagile/UserRepository.java
+++ src/main/java/com/taskagile/UserRepository.java
@@ -1,7 +1,10 @@
 package com.taskagile;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
   User findByUsername(String username);
   User findByEmailAddress(String emailAddress);
-  void save(User user);
 }
